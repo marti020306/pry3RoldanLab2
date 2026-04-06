8254507f9144241995b18d4ba143e351feae0ec2
Alumno <Alumno@PC34>
2026-04-06 12:00:42 -0300

    Agregar archivos de proyecto.

---
+++ pry3RoldanLab2/frmGestion.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace pry3RoldanLab2
+{
+    partial class frmGestion
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grbCarga = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.lblDeuda = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.mtbDeuda = new System.Windows.Forms.MaskedTextBox();
+            this.mtbCodigo = new System.Windows.Forms.MaskedTextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.mtbLimite = new System.Windows.Forms.MaskedTextBox();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.grbCarga.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grbCarga
+            // 
+            this.grbCarga.Controls.Add(this.btnCargar);
+            this.grbCarga.Controls.Add(this.mtbLimite);
+            this.grbCarga.Controls.Add(this.txtUsuario);
+            this.grbCarga.Controls.Add(this.mtbCodigo);
+            this.grbCarga.Controls.Add(this.mtbDeuda);
+            this.grbCarga.Controls.Add(this.label4);
+            this.grbCarga.Controls.Add(this.lblUsuario);
+            this.grbCarga.Controls.Add(this.lblDeuda);
+            this.grbCarga.Controls.Add(this.lblCodigo);
+            this.grbCarga.Location = new System.Drawing.Point(12, 12);
+            this.grbCarga.Name = "grbCarga";
+            this.grbCarga.Size = new System.Drawing.Size(457, 206);
+            this.grbCarga.TabIndex = 2;
+            this.grbCarga.TabStop = false;
+            this.grbCarga.Text = "Carga De Datos";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 234);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(457, 277);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Consulta De Datos";
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigo.Location = new System.Drawing.Point(9, 41);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(51, 16);
+            this.lblCodigo.TabIndex = 0;
+            this.lblCodigo.Text = "Codigo";
+            // 
+            // lblDeuda
+            // 
+            this.lblDeuda.AutoSize = true;
+            this.lblDeuda.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDeuda.Location = new System.Drawing.Point(9, 102);
+            this.lblDeuda.Name = "lblDeuda";
+            this.lblDeuda.Size = new System.Drawing.Size(48, 16);
+            this.lblDeuda.TabIndex = 1;
+            this.lblDeuda.Text = "Deuda";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.Location = new System.Drawing.Point(245, 46);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(54, 16);
+            this.lblUsuario.TabIndex = 2;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(215, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Limite De Credito";
+            // 
+            // mtbDeuda
+            // 
+            this.mtbDeuda.Location = new System.Drawing.Point(66, 102);
+            this.mtbDeuda.Mask = "9999999";
+            this.mtbDeuda.Name = "mtbDeuda";
+            this.mtbDeuda.Size = new System.Drawing.Size(100, 20);
+            this.mtbDeuda.TabIndex = 4;
+            this.mtbDeuda.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.mtbDeuda_MaskInputRejected);
+            // 
+            // mtbCodigo
+            // 
+            this.mtbCodigo.Location = new System.Drawing.Point(66, 41);
+            this.mtbCodigo.Mask = "99999";
+            this.mtbCodigo.Name = "mtbCodigo";
+            this.mtbCodigo.Size = new System.Drawing.Size(100, 20);
+            this.mtbCodigo.TabIndex = 5;
+            this.mtbCodigo.ValidatingType = typeof(int);
+            this.mtbCodigo.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.mtbCodigo_MaskInputRejected);
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(314, 42);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(116, 20);
+            this.txtUsuario.TabIndex = 6;
+            this.txtUsuario.TextChanged += new System.EventHandler(this.txtUsuario_TextChanged);
+            // 
+            // mtbLimite
+            // 
+            this.mtbLimite.Location = new System.Drawing.Point(330, 102);
+            this.mtbLimite.Mask = "99999999";
+            this.mtbLimite.Name = "mtbLimite";
+            this.mtbLimite.Size = new System.Drawing.Size(100, 20);
+            this.mtbLimite.TabIndex = 7;
+            this.mtbLimite.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.mtbLimite_MaskInputRejected);
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Enabled = false;
+            this.btnCargar.Location = new System.Drawing.Point(289, 161);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(141, 23);
+            this.btnCargar.TabIndex = 8;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(427, 241);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Codigo";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Usuario";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Deuda";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Limite";
+            this.Column4.Name = "Column4";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(9, 533);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(103, 16);
+            this.lblTotal.TabIndex = 3;
+            this.lblTotal.Text = "Total De Deuda";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(118, 532);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 4;
+            this.maskedTextBox1.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
+            // 
+            // btnListar
+            // 
+            this.btnListar.Enabled = false;
+            this.btnListar.Location = new System.Drawing.Point(335, 526);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(116, 23);
+            this.btnListar.TabIndex = 5;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // frmGestion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 565);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.grbCarga);
+            this.Name = "frmGestion";
+            this.Text = "Gestion De Clientes";
+            this.Load += new System.EventHandler(this.frmGestion_Load);
+            this.grbCarga.ResumeLayout(false);
+            this.grbCarga.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox grbCarga;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.MaskedTextBox mtbCodigo;
+        private System.Windows.Forms.MaskedTextBox mtbDeuda;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblDeuda;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.MaskedTextBox mtbLimite;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button btnListar;
+    }
+}
+
---
+++ pry3RoldanLab2/frmGestion.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pry3RoldanLab2
+{
+    public partial class frmGestion : Form
+    {
+        public frmGestion()
+        {
+            InitializeComponent();
+        }
+
+        //Declarar Registro Cliente
+        private struct RegCliente
+        {
+            public Int32 Codigo;
+            public string Usuario;
+            public Decimal Deuda;
+            public Decimal Limite;
+        }
+
+        //Declarar Vector
+
+       private RegCliente[] Cliente = new RegCliente[10];
+
+        //Declarar Indice
+
+        private Int32 Indice = 0;
+
+
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            if (Indice < Cliente.Length)
+            {
+                Cliente[Indice].Codigo = Convert.ToInt32(mtbCodigo.Text);
+                Cliente[Indice].Usuario = txtUsuario.Text;
+                Cliente[Indice].Deuda = Convert.ToDecimal(mtbDeuda.Text);
+                Cliente[Indice].Limite = Convert.ToDecimal(mtbLimite.Text);
+                MessageBox.Show("Cliente Cargado");
+                Indice++;
+                Limpiar();
+            }
+            else
+            {
+                MessageBox.Show("No se pueden cargar mas clientes");
+            }
+
+        }
+
+        private void Limpiar()
+        {
+            mtbCodigo.Clear();
+            txtUsuario.Clear();
+            mtbDeuda.Clear();
+            mtbLimite.Clear();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+
+        {
+            Decimal TotalDeuda = 0;
+
+            for (int i = 0; i < Indice; i++)
+            {
+                dataGridView1.Rows.Add(Cliente[i].Codigo, Cliente[i].Usuario, Cliente[i].Deuda, Cliente[i].Limite);
+                TotalDeuda += Cliente[i].Deuda;
+            }
+           maskedTextBox1.Text = TotalDeuda.ToString();
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void frmGestion_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Desbloquear()
+        {
+            if (mtbCodigo.Text != "" && mtbDeuda.Text != "" && txtUsuario.Text != "" && mtbLimite.Text != "")
+            {
+                btnCargar.Enabled = true;
+                btnListar.Enabled = true;
+            }
+            else
+            {
+                btnListar.Enabled = false;
+                btnCargar.Enabled = false;
+            }
+        }
+
+        private void mtbCodigo_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            Desbloquear();
+        }
+
+        private void txtUsuario_TextChanged(object sender, EventArgs e)
+        {
+            Desbloquear();
+        }
+
+        private void mtbDeuda_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            Desbloquear();
+        }
+
+        private void mtbLimite_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            Desbloquear();
+        }
+    }
+    
+
+}
